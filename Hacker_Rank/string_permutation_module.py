66cba6503353b656869bf5075adb9101513e026e
Harsh Gaur <harshgaur997@gmail.com>
2020-02-11 18:35:05 +0530

    new

--- Hacker_Rank/string_permutation_module.py
+++ Hacker_Rank/string_permutation_module.py
@@ -1,3 +1,15 @@
+import sys
 from itertools import permutations
 
-print(list(permutations('Hack',2)))
+def permu(stri,num):
+    for i in list(permutations(stri,num)):
+        j= list(i)
+        for l,k in enumerate(j):
+            j[l]=k.upper()
+        j.sort()
+        sys.stdout.write("".join(j)+"\n")
+
+
+line1 = sys.stdin.readline()
+n=line1.split(" ")
+permu(n[0],int(n[1]))
